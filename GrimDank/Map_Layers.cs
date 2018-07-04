2cf259ed7dccb25678aa5ed13376520ed303155e
Chris Ridley <cridley106@gmail.com>
2018-07-04 14:35:13 -0500

    Made layer list more maintainable.  Was actually easier than making it completely static for the moment so yeah.

--- GrimDank/Map_Layers.cs
+++ GrimDank/Map_Layers.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Collections.Generic;
 
 namespace GrimDank
 {
@@ -12,11 +13,18 @@ namespace GrimDank
         public enum Layer
         { ITEMS, CREATURES }
 
-        // Array saying whether each of the layers in the Layers enum can have multiple items on that layer
-        // or not (ones like creature that never should allow it, should say so, for optimization reasons.  Should always be the same size as
-        // the Layers array and it will fail horribly at runtime if its not.
-        private static readonly bool[] _layerCanHaveMultipleItems =
-        { true, false };
+        // Each layer must have an entry in this dictionary, specifying whether or not that layer allows multiple items on that layer
+        // to occupy the same space (basically whether or not it uses buckets).  Items, for example should -- creatures, not so much.
+        // Pretty much all the code that has to care about this has already been written, but if you're trying to check whether its allowed
+        // at run-time, use the bool array below -- its generated based on these values
+        private static readonly Dictionary<Layer, bool> _layerMultipleItemsMap = new Dictionary<Layer, bool>
+        {
+            { Layer.ITEMS, true },
+            { Layer.CREATURES, false }
+        };
+
+        // Dictionary is only used above for ease of maintenance, this is used to check in production code
+        private static readonly bool[] _layerCanHaveMultipleItems;
 
         private static readonly int _layerSize;
 
@@ -27,15 +35,19 @@ namespace GrimDank
         {
             _layerSize = Enum.GetNames(typeof(Layer)).Length;
 
-            // Debug call only
+            // Called in Debug mode only.
             TestAssociations();
+
+            _layerCanHaveMultipleItems = new bool[_layerSize];
+            foreach (var kvPair in _layerMultipleItemsMap)
+                _layerCanHaveMultipleItems[(int)kvPair.Key] = kvPair.Value;
         }
 
         [Conditional("DEBUG")]
         private static void TestAssociations()
         {
-            // If this fails, the length of the boolean array isn't corresponding 1 to 1 to the Layers enum.
-            Debug.Assert(_layerCanHaveMultipleItems.Length == _layerSize);
+            // If this fails, the number of Dictionary entries isn't corresponding 1 to 1 to the Layers enum.
+            Debug.Assert(_layerMultipleItemsMap.Count == _layerSize);
 
             // If one of these fails, enum values aren't default (you've done something like SOMELAYER = 4 in the Layer declaration and that will
             // break things
