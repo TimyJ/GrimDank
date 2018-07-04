9d878355921d916832794c6f9d22842a339704b6
Chris Ridley <cridley106@gmail.com>
2018-07-03 20:38:43 -0500

    Added mostly blank classes for Map and MObject

---
+++ GrimDank/MObjects/MObject.cs
@@ -0,0 +1,19 @@
+﻿using GoRogue;
+
+namespace GrimDank.MObjects
+{
+    class MObject : IHasID
+    {
+        // This will pretty much be unsat later as far as serialization goes but easy enough to sort out then
+        private static readonly IDGenerator _idGen = new IDGenerator();
+
+        public uint ID { get; private set; }
+
+        // Can add parameters just here for ID stuff.
+        public MObject()
+        {
+            ID = _idGen.UseID();
+        }
+
+    }
+}
