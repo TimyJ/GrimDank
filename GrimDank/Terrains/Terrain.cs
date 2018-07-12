8a54dcd002c16ef8eb2cca7f9bfeccfe5b97cda6
Chris Ridley <cridley106@gmail.com>
2018-07-11 20:15:06 -0500

    Added a little more docs.

--- GrimDank/Terrains/Terrain.cs
+++ GrimDank/Terrains/Terrain.cs
@@ -1,6 +1,8 @@
 ﻿namespace GrimDank.Terrains
 {
-    // Partial class -- Terrain_Types.cs contains the static instances for each terrain type.
+    // Partial class -- Terrain_Types.cs contains the static instances for each terrain type.  To enforce that they are static instances,
+    // constructor is private so only Terrain can create instances -- the intent is to force all instances to be made in Terrain_Types.cs
+    // so that we don't accidentally create instances somewhere.
     partial class Terrain
     {
         public bool IsWalkable { get; set; }
