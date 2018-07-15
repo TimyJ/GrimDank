3b4d2ee034d60b7eda23ff53ff95188680df1e02
Chris Ridley <cridley106@gmail.com>
2018-07-15 14:08:15 -0500

    Added extension methods to appropraite classes, to convert between XNA Point/GoRogue Coord, and XNA Rectangle/GoRogue Rectangle.

---
+++ GrimDank/IntegrationExtensions.cs
@@ -0,0 +1,19 @@
+﻿using GoRogue;
+using Microsoft.Xna.Framework;
+using XNARect = Microsoft.Xna.Framework.Rectangle;
+
+namespace GrimDank
+{
+    // Extensions for various MonoGame and GoRogue classes pertaining to integrating the two libraries, particularly
+    // where they both have types representing different things.
+    static class IntegrationExtensions
+    {
+        // Convert between point representations
+        public static Coord ToCoord(this Point p) => Coord.Get(p.X, p.Y);
+        public static Point ToPoint(this Coord c) => new Point(c.X, c.Y);
+
+        // Convert between rectangle representations
+        public static GoRogue.Rectangle ToGoRogueRect(this XNARect r) => new GoRogue.Rectangle(r.X, r.Y, r.Width, r.Height);
+        public static XNARect ToXNARect(this GoRogue.Rectangle r) => new XNARect(r.X, r.Y, r.Width, r.Height);
+    }
+}
